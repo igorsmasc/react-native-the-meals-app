876c0edeef4fb251c772d1227fdd49c603c0d3ed
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-06-28 19:59:45 -0300

    Add tabs-based navigation

--- routes/MealsNavigator.js
+++ routes/MealsNavigator.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoriesMeals from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const MealsNavigator = () => {
   return (
@@ -26,4 +30,27 @@ const MealsNavigator = () => {
   );
 };
 
-export default MealsNavigator;
+const MealsFavTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Meals"
+      tabBarOptions={{
+        activeTintColor: Colors.accent,
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          let iconName;
+
+          iconName = route.name === 'Meals' ? 'ios-restaurant' : 'ios-star';
+
+          return <Ionicons name={iconName} size={25} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Meals" component={MealsNavigator} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default MealsFavTabNavigator;
