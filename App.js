29d229d0f8847f8d0363e278a6d92fbfefde20c3
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-06-29 19:10:34 -0300

    Add filters screen and redux

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import { NavigationContainer } from '@react-navigation/native';
 import MealsNavigator from './routes/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -30,8 +39,10 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <MealsNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <MealsNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,10 +1,46 @@
-import React, { useLayoutEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {
+  useLayoutEffect,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
+import { View, Text, StyleSheet, Switch } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
 
-const FiltersScreen = ({ screen, navigation }) => {
+const FilterSwitch = ({ label, state, onChange }) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primary }}
+        thumbColor={Colors.primary}
+        value={state}
+        onValueChange={onChange}
+      />
+    </View>
+  );
+};
+
+const FiltersScreen = ({ screen, navigation, route }) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const saveFilters = useCallback(() => {
+    const appliedFilters = {
+      isGlutenFree: isGlutenFree,
+      isLactoseFree: isLactoseFree,
+      isVegan: isVegan,
+      isVegetarian: isVegetarian,
+    };
+
+    console.log(appliedFilters);
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Filter Meals',
@@ -19,12 +55,49 @@ const FiltersScreen = ({ screen, navigation }) => {
           />
         </HeaderButtons>
       ),
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item title="Menu" iconName="ios-save" onPress={route.params.save} />
+        </HeaderButtons>
+      ),
     });
-  }, [navigation]);
+  }, [navigation, route]);
+
+  useEffect(() => {
+    navigation.setParams({ save: saveFilters });
+  }, [navigation, saveFilters]);
 
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screen!</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={(newValue) => {
+          setIsGlutenFree(newValue);
+        }}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={(newValue) => {
+          setIsLactoseFree(newValue);
+        }}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={(newValue) => {
+          setIsVegan(newValue);
+        }}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={(newValue) => {
+          setIsVegetarian(newValue);
+        }}
+      />
     </View>
   );
 };
@@ -32,8 +105,20 @@ const FiltersScreen = ({ screen, navigation }) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center',
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+    marginVertical: 15,
   },
 });
 
